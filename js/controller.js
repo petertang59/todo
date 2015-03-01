1fa13c242576d713f3580b900fc9fa29189ea48e
petertang59 <petertang59@gmail.com>
2015-02-28 17:50:08 -0800

    asd

---
+++ js/controller.js
@@ -0,0 +1,34 @@
+// var TodoApp = angular.module('TodoApp', ['ngAnimate']);
+
+// TodoApp.controller('todoController',['$scope',function($scope){
+// 	$scope.todos = [
+// 		{'title':'Build a todo app', 'done':false, 'hour':'00:','minute':'00'}
+// 	];
+
+// 	$scope.orderProp='date';
+   
+//    //  $scope.sortDate = function (tabIndex) {
+//    //   //Sort by date
+//    //      $scope.orderProp='date';    
+//    //    }   
+//    // };
+	
+// 	// $scope.details = [
+// 	// 	{'title':'Before the due date', 'done':false}
+// 	// ];
+
+// 	$scope.addTodo = function (){
+// 		$scope.todos.push({'title':$scope.newToDo,'detail':$scope.newDetail,'hour':$scope.newHour,'minute':$scope.newMinute,'done':false})
+// 		$scope.newToDo = ""
+// 			$scope.newDetail = ""
+// 			$scope.newHour = ""
+// 			$scope.newMinute = ""
+// 	}	
+
+// 	$scope.clearCompleted = function (){
+// 		$scope.todos = $scope.todos.filter(function(item) {
+// 			return !item.done
+// 		})
+// 	}
+
+// }])
